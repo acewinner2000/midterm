3733896f52305423bf13d966ae974a02fc24c125
acewinner2000 <ali.ghiasabadi.2000@gmail.com>
2024-08-01 17:40:24 +0330

    1

--- root/views.py
+++ root/views.py
@@ -3,5 +3,5 @@ from .models import Serivces
 
 
 def home(request):
-    services=Serivces.object.filter(status=True)
+    services=Serivces.objects.filter(status=True)
     return render(request,'root/index.html',context={'services':Serivces})
